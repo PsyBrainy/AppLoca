dd38aa68f1e272e307c34fc034e47d82543ed6aa
Ronan <ronan.lavorato@gmail.com>
2022-09-24 02:40:11 -0300

    npm-icon y Login de mui, importacion de ruta

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@
 import React from 'react';
 import CuadroChat from "./components/CuadroChatComponent.jsx"
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ThemeProvider from "./components/SignIn.jsx"
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<CuadroChat />} />
+      </Routes>
+      <Routes>
+        <Route path="/1" element={<ThemeProvider />} />
       </Routes>
     </BrowserRouter>
   );
