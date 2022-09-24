ca115ab010891c4d97beb3f7a883f410556742dd
Ronan <ronan.lavorato@gmail.com>
2022-09-24 00:05:37 -0300

    se soluciona toda la mierda

--- src/App.js
+++ src/App.js
@@ -1,32 +1,9 @@
-
-<<<<<<< HEAD
-import React,{ useEffect , useState } from 'react';
-import CardContent from '@mui/material/CardContent';
-import { Container } from '@mui/system';
-import { saveMensaje } from './service/mensajesService';
-import FeedMensajesComponent from './components/FeedMensajesComponent';
-import PopMensajeComponent from './components/PopMensajeComponent';
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
-import './style.css';
-=======
+import './styles/style.css';
 import React from 'react';
-<<<<<<< HEAD
-import CuadroChatComponent from "./components/CuadroChatComponent"
->>>>>>> 6beb7125ced4cb0daccffb8bdeb6e9aaf37543e8
-=======
+
 import CuadroChatComponent from "./components/CuadroChatComponent.jsx"
 
->>>>>>> 9099b31cc9df6667b9fb178ce6a063caef196308
-
 function App() {
-
-
 
   return(
     <>
